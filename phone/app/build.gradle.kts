f8a46e19ece88e78b1a64f76491fd6d04af7b0f1
李x <xjk3251745883@gmail.com>
2026-06-13 11:48:50 +0800

    chore: 添加 RecyclerView 依赖和基础资源

--- phone/app/build.gradle.kts
+++ phone/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
